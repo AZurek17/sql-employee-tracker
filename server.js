241cae992591c560cba8c5315ad5f5e5ebb60c4b
Andy <84168944+AZurek17@users.noreply.github.com>
2023-10-27 18:58:42 -0700

    added mysql connection to server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const inquirer = require("inquirer");
+const mysql = require('mysql2');
+
+const PORT = 3001;
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'company_db'
+    },
+    console.log('connected to company_db')
+);
+
+// query database
+
+
